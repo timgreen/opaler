c52ab03cfbeab99da06be08c242f96fa54076b9a
Tim Green <iamtimgreen@gmail.com>
2015-07-27 14:38:04 +1000

    Replace rating dialog.

--- android/main/src/main/scala/it/timgreen/opal/RateSupport.scala
+++ android/main/src/main/scala/it/timgreen/opal/RateSupport.scala
@@ -3,24 +3,13 @@ package it.timgreen.opal
 import android.app.Activity
 import android.os.Bundle
 
-import com.github.fernandodev.easyratingdialog.library.EasyRatingDialog
+import hotchemi.android.rate.AppRate
 
 trait RateSupport { self: Activity =>
 
-  private var easyRatingDialog: Option[EasyRatingDialog] = _
-
   override def onCreate(savedInstanceState: Bundle) {
     self.onCreate(savedInstanceState)
-    easyRatingDialog = Some(new EasyRatingDialog(self))
-  }
-
-  override def onStart() {
-    self.onStart
-    easyRatingDialog foreach { _.onStart }
-  }
-
-  override def onResume() {
-    self.onResume
-    easyRatingDialog foreach { _.showIfNeeded }
+    AppRate.`with`(self).monitor
+    AppRate.showRateDialogIfMeetsConditions(this);
   }
 }
